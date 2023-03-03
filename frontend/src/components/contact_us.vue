5dc2d537fc31300d338ce03754b28ce6ce070c9d
BhavyaSingh-Rolly <bhsingh@ucsd.edu>
2023-03-02 21:51:58 -0800

    Footer Pages

---
+++ frontend/src/components/contact_us.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="contactus">
+        <div id="contactusbox">
+            <h1> Contact Us </h1> <br>
+            <h2> Email Address</h2> 
+            <span> roomieorganisation@gmail.com </span> <br> <br>
+            <h2> Mailing Address </h2> 
+            <span> 9500 Gilman Drive, Mail Code 0404 <br> La Jolla, CA 92093-0404 USA</span>
+            <br> <br>
+        </div>
+    </div>
+</template>
+
+<style>
+
+#contactus {
+  min-height: 100vh; 
+  margin-bottom: 50px;
+}
+
+#contactusbox {
+  width: 50%;
+  margin: auto;
+  border: 2px solid rgb(132, 188, 132);
+  border-radius: 25px;
+  background: rgb(246, 245, 245);
+  padding: 25px;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  left: 50%;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,11 @@ import OTPSignup from "../components/otp_signup.vue";
 import OTPForgotPassword from "../components/otp_forgot_password.vue";
 import TempSearchResults from "../components/temp_search_results.vue";
 import Connections from "../components/connections.vue";
+import AboutUs from "../components/about_us.vue";
+import ContactUs from "../components/contact_us.vue";
+import PrivacyPolicy from "../components/privacy_policy.vue";
+import TermsConditions from "../components/terms_conditions.vue";
+
 const routes=[
     {
         path: "/login",
@@ -59,6 +64,26 @@ const routes=[
         path: "/temp",
         name: "temp",
         component:TempSearchResults,
+    },
+    {
+        path: "/about_us",
+        name:"about_us",
+        component:AboutUs,
+    }, 
+    {
+        path: "/contact_us",
+        name:"contact_us",
+        component:ContactUs,
+    },
+    {
+        path: "/privacy_policy",
+        name:"privacy_policy",
+        component:PrivacyPolicy,
+    },
+    {
+        path: "/terms_conditions",
+        name:"terms_conditions",
+        component:TermsConditions,
     },
 ];
 const router=createRouter({
